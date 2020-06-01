bbb6af3fe191448df3f507151476e7111a74ce84
Vishal Ranjan <vishivish18@gmail.com>
2020-06-02 03:52:03 +0530

    adding cron

--- scripts/cron.py
+++ scripts/cron.py
@@ -5,4 +5,5 @@
 import requests
 
 
-res_data = requests.get('http://localhost:8000/crawler')
+res_data = requests.get('http://crawler.coronalogy.xyz/crawler')
+#30 4 * * * /Users/vishalranjan/opt/anaconda3/bin/python /Users/vishalranjan/projects/covid-19/scripts/cron.py
