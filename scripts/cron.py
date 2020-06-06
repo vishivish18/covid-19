77ca811e0dc7f8db2f1e2491f2587deebb101e22
Vishal Ranjan <vishivish18@gmail.com>
2020-06-06 00:43:56 +0000

    sync from server

--- scripts/cron.py
+++ scripts/cron.py
@@ -6,4 +6,3 @@ import requests
 
 
 res_data = requests.get('http://crawler.coronalogy.xyz/crawler')
-#30 4 * * * /Users/vishalranjan/opt/anaconda3/bin/python /Users/vishalranjan/projects/covid-19/scripts/cron.py
